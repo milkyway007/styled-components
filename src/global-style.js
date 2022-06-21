a751cc2ef848c7a46631b124e4b075dea1c9bde4
Liidia Laada <liidia@sixfold.com>
2022-06-21 11:38:05 +0300

    Theming

--- src/global-style.js
+++ src/global-style.js
@@ -16,6 +16,19 @@ const GlobalStyle = createGlobalStyle`
    -moz-osx-font-smoothing: grayscale;
    background: #f2f4f8;
  }
+
+ .btn {
+  background: var(--primary);
+  color: #fff;
+  border: none;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  text-transform: capitalize;
+  padding: 0.25rem;
+  display: block;
+  width: 200px;
+  margin: 1rem auto;
+}
 `;
 
 export default GlobalStyle;
