2bb611322df732615b0c496a3c16830b645c40f3
Liidia Laada <liidia@sixfold.com>
2022-07-11 13:33:43 +0300

    More complex props, list example

--- src/App-6.js
+++ src/App-6.js
@@ -1,9 +1,9 @@
-import List from './components/List';
+import Form from './components/Form';
 
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
-      <List />
+      <Form />
     </div>
   );
 }
