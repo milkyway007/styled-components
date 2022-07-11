29455afe2476b9672ce84d4a60d248c1fe2e8424
Liidia Laada <liidia@sixfold.com>
2022-07-11 15:26:49 +0300

    More complex props; products example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import List from './components/List';
+import Products from './components/Products';
 
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
-      <List />
+      <Products />
     </div>
   );
 }
